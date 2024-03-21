0fab8127a7d0fe4ccb996a3ceda9baa29825b8b6
syed fahad ali <155745867+syedfahadali399@users.noreply.github.com>
2024-03-21 20:01:16 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operator to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);    
+} else if (answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);  
+} else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);  
+} else if (answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);  
+} else {
+    console.log("Please Select Valid Operator"); 
+}
+
